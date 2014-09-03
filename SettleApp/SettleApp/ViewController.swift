83c62ab82c449235dc09acd87968bffdd2df0ed2
Tobias Lundgren <tobias.l@me.com>
2014-09-03 15:20:24 +0200

    Test upload

---
+++ SettleApp/SettleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SettleApp
+//
+//  Created by Tobias Lundgren on 2014-09-03.
+//  Copyright (c) 2014 Tobias Lundgren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
